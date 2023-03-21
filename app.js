e5bd0951be74611b8ec3ea77200e538b5a032be9
Hayden Howle <hayden.howle@teambespin.us>
2023-03-21 10:59:40 -0500

    nodemon added

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/random", (req, res) => {
+  res.send("lmao");
+});
+
+app.get("*", (req, res) => {
+  res.send("doesn't exist");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });

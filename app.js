498ee66151d2623f818a2b42f474a4f9f46bd208
Hayden Howle <hayden.howle@teambespin.us>
2023-03-21 11:21:06 -0500

    live reload init

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const livereload = require("livereload");
+const path = require("path");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+if (app.get("env") === "development") {
+  const liveReloadServer = livereload.createServer();
+  liveReloadServer.watch(path.join(__dirname, "public"));
+
+  app.use(require("connect-livereload")());
+
+  liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+      liveReloadServer.refresh("/");
+    }, 100);
+  });
+}
+
+const homeDir = ["/", "/home"];
+app.get(homeDir, (req, res) => {
   res.send("Hello World!");
 });
 

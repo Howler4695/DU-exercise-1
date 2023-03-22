2fa58219b2a5835cf7d87c2299768d91913e00cc
Hayden Howle <hayden.howle@teambespin.us>
2023-03-22 10:51:05 -0500

    backend reading formData

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const livereload = require("livereload");
 // const rootRoutes = require("./router.js");
 // const accountRoutes = require("./account.js");
 const path = require("path");
+const { json } = require("body-parser");
 const app = express();
 const port = 3000;
 
@@ -20,6 +22,18 @@ if (app.get("env") === "development") {
 }
 
 app.use(express.static(path.join(__dirname, "public")));
+
+const jsonParser = bodyParser.json();
+
+app.post("/form", jsonParser, (req, res) => {
+  console.log(`Received data at /form: ${JSON.stringify(req.body)}`);
+  res.status(201);
+  res.send("OK");
+});
+
+app.get("/form", (req, res) => {
+  res.send("OK");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,36 @@
-const handleFormSubmition = (event) => {
+const handleFormSubmition = async (event) => {
   event.preventDefault();
 
   const formData = new FormData(event.target);
-  const articleText = formData.get("article-text");
-  const authorName = formData.get("author-name");
+  const formEntries = Object.fromEntries(formData);
 
-  console.log(articleText, authorName);
+  const res = await fetch("/form", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formEntries),
+  });
+  console.log(res);
+  if (res.status === 201) {
+    articleAdd(articleTitle, articleText, authorName);
+  } else if (res.status === 200) {
+    console.log("Article seen but not heard");
+  } else {
+    console.log(`Something went wrong ${res.statusCode}`);
+  }
 };
 
-const articleAdd = (articleText, authorName) => {
-  const newArticleElement = document.createElement("article");
+const articleAdd = (articleTitle, articleText, authorName) => {
+  const newestArticle = document.querySelector("#articles");
+  const newSection = document.createElement("section");
+  const newArticle = document.createElement("article");
+  const newArticleTitle = document.createElement("h1");
+  const newArticleText = document.createElement("p");
+  document.createTextNode(newArticleText).appendChild(newArticleText);
+
+  console.log(newestArticle);
+  console.log(newArticleText);
 };
 
 const form = document.getElementById("form");
